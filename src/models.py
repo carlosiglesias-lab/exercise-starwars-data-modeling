84f04b68085c81da2024a85e01babfcef135a480
carlosiglesias-lab <carlos.igles@gmail.com>
2023-01-18 14:00:07 +0000

    v18/01

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,59 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    url_img = Column(String(250))
+    alive = Column(Boolean)
+
+class Episodes(Base):
+    __tablename__ = 'episodes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lenght = Column(Integer)
+
+class Locations(Base):
+    __tablename__ = 'locations'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    type = Column(String(250))
+
+class Characters_episodes(Base):
+    __tablename__ = 'characters_episodes'
+    id = Column(Integer, primary_key=True)
+    id_character = Column(Integer, ForeignKey('characters.id'))
+    id_episode = Column(Integer, ForeignKey('episodes.id'))
+    character = relationship(Characters)
+    episode = relationship(Episodes)
+
+class Episodes_locations(Base):
+    __tablename__ = 'episodes_locations'
+    id = Column(Integer, primary_key=True)    
+    id_episode = Column(Integer, ForeignKey('episodes.id'))
+    id_location = Column(Integer, ForeignKey('locations.id'))
+    episode = relationship(Episodes)
+    location = relationship(Locations)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_character = Column(Integer,ForeignKey('characters.id'))
+    id_episode = Column(Integer, ForeignKey('episodes.id'))
+    id_location = Column(Integer, ForeignKey('locations.id'))
+    user = relationship(User)
+    character = relationship(Characters)
+    episode = relationship(Episodes)
+    location = relationship(Locations)
+
 
     def to_dict(self):
         return {}
